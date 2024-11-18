9caa804c4912df00cb1d35bf5e1f931c07118dda
Tausif Meah <tausifmeah@gmail.com>
2024-11-18 08:42:09 +0000

    revert

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,101 +1,88 @@
 import React, { useState } from "react";
-import { HashLink as Link } from "react-router-hash-link";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  Container,
-} from "reactstrap";
+import { HashLink as Link } from "react-router-hash-link"; // import HashLink from react-router-hash-link
 import logo from "../images/sjclogo.png";
 import "../styles/nav.css";
 import ContactForm from "./ContactForm";
+// import { Container } from "react-bootstrap";
 
 function Navigation() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <Navbar expand="lg" className="navbar-div">
-      <Container>
-        <NavbarBrand className="nav-logo-div">
-          <img src={logo} alt="" className="nav-logo" />
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNav} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="navbar-links" navbar>
-            <NavItem>
-              <NavLink
-                tag={Link}
-                to="/"
-                smooth
-                className="navbar-link link__hover--effect link__hover--effect--white"
-              >
-                Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                tag={Link}
-                to="/#services"
-                smooth
-                className="navbar-link link__hover--effect link__hover--effect--white"
-              >
-                Services
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                tag={Link}
-                to="/#about-us"
-                smooth
-                className="navbar-link link__hover--effect link__hover--effect--white"
-              >
-                About Us
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                tag={Link}
-                to="/gallery"
-                smooth
-                className="navbar-link link__hover--effect link__hover--effect--white"
-              >
-                Our Work
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                tag={Link}
-                to="/#contact-us"
-                onClick={toggleModal}
-                smooth
-                className="navbar-link link__hover--effect link__hover--effect--white"
-              >
-                Contact Us
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Container>
+    <div className="navbar-div">
+      <div className="nav-logo-div">
+        <img src={logo} alt="" className="nav-logo" />
+      </div>
+      <div className="navbar-links-div">
+        <ul className="navbar-links">
+          <li>
+            <Link
+              to="/"
+              smooth
+              className="navbar-link link__hover--effect link__hover--effect--white"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/#services"
+              smooth
+              className="navbar-link link__hover--effect link__hover--effect--white"
+            >
+              Services
+            </Link>
+          </li>
 
+          <li>
+            <Link
+              to="/#about-us"
+              smooth
+              className="navbar-link link__hover--effect link__hover--effect--white"
+            >
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/gallery"
+              smooth
+              className="navbar-link link__hover--effect link__hover--effect--white"
+            >
+              Our Work
+            </Link>
+          </li>
+
+          {/* <li>
+            <Link
+              to="/shop"
+              smooth
+              className="navbar-link link__hover--effect link__hover--effect--white"
+            >
+              Shop
+            </Link>
+          </li> */}
+          <li>
+            <Link
+              to="/#contact-us"
+              onClick={toggleModal}
+              smooth
+              className="navbar-link link__hover--effect link__hover--effect--white"
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
       <div className={`modal ${isModalOpen ? "modal--open" : ""}`}>
         {isModalOpen && (
           <ContactForm onClose={toggleModal} isModalOpen={isModalOpen} />
         )}
       </div>
-    </Navbar>
+    </div>
   );
 }
 
